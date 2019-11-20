5680418405295cb9e7724f4716baeb05c80778bb
deefunkt <deefunktify@gmail.com>
2019-11-20 15:07:58 +1100

    added context definitions for 64bit processes

--- grey hat python/my_debugger_defines.py
+++ grey hat python/my_debugger_defines.py
@@ -4,6 +4,7 @@ from ctypes import *
 BYTE      = c_ubyte
 WORD      = c_ushort
 DWORD     = c_ulong
+DWORD64   = c_ulonglong
 LPBYTE    = POINTER(c_ubyte)
 LPTSTR    = POINTER(c_char) 
 HANDLE    = c_void_p
@@ -177,9 +178,9 @@ class FLOATING_SAVE_AREA(Structure):
         ("Cr0NpxState", DWORD),
 ]
 
-# The CONTEXT structure which holds all of the 
+# The 32 bit CONTEXT structure which holds all of the 
 # register values after a GetThreadContext() call
-class CONTEXT(Structure):
+class WOW64CONTEXT(Structure):
     _fields_ = [
         ("ContextFlags", DWORD),
         ("Dr0", DWORD),
@@ -266,4 +267,112 @@ class MEMORY_BASIC_INFORMATION(Structure):
         ("Protect", DWORD),
         ("Type", DWORD),
 ]
-    
+
+####################################################
+#####  Below are definitions for 64 bit processors
+####################################################
+class M128A(Structure):
+    _fields_ = [
+        ("Low", DWORD64),
+        ("High", DWORD64)
+    ] 
+
+class XMM_SAVE_AREA32(Structure):
+    _pack_ = 1 
+    _fields_ = [  
+        ('ControlWord', WORD), 
+        ('StatusWord', WORD), 
+        ('TagWord', BYTE), 
+        ('Reserved1', BYTE), 
+        ('ErrorOpcode', WORD), 
+        ('ErrorOffset', DWORD), 
+        ('ErrorSelector', WORD), 
+        ('Reserved2', WORD), 
+        ('DataOffset', DWORD), 
+        ('DataSelector', WORD), 
+        ('Reserved3', WORD), 
+        ('MxCsr', DWORD), 
+        ('MxCsr_Mask', DWORD), 
+        ('FloatRegisters', M128A * 8), 
+        ('XmmRegisters', M128A * 16), 
+        ('Reserved4', BYTE * 96)
+    ]
+
+class DUMMYSTRUCTNAME(Structure):
+    _fields_ = [
+        ('Header', M128A*2),
+        ('Legacy', M128A*8),
+        ('Xmm0', M128A),
+        ('Xmm1', M128A),
+        ('Xmm2', M128A),
+        ('Xmm3', M128A),
+        ('Xmm4', M128A),
+        ('Xmm5', M128A),
+        ('Xmm6', M128A),
+        ('Xmm7', M128A),
+        ('Xmm8', M128A),
+        ('Xmm9', M128A),
+        ('Xmm10', M128A),
+        ('Xmm11', M128A),
+        ('Xmm12', M128A),
+        ('Xmm13', M128A),
+        ('Xmm14', M128A),
+        ('Xmm15', M128A),
+    ]
+class DUMMYUNIONNAME(Union):
+            _fields_ = [
+                ('FltSave', XMM_SAVE_AREA32),
+                ('Q', NEON128*16),
+                ('D', ULONGLONG*32),
+                ('S', DWORD*32),
+            ]
+# The 64 bit CONTEXT structure which holds all of the 
+# register values after a GetThreadContext() call
+class CONTEXT(Structure):
+   _fields_ = [
+        ('P1Home', DWORD64),
+        ('P2Home', DWORD64),
+        ('P3Home', DWORD64),
+        ('P4Home', DWORD64),
+        ('P5Home', DWORD64),
+        ('P6Home', DWORD64),
+        ('ContextFlags', DWORD),
+        ('MxCsr', DWORD),
+        ('SegCs', WORD),
+        ('SegDs', WORD),
+        ('SegEs', WORD),
+        ('SegFs', WORD),
+        ('SegGs', WORD),
+        ('SegSs', WORD),
+        ('EFlags', DWORD),
+        ('Dr0', DWORD64),
+        ('Dr1', DWORD64),
+        ('Dr2', DWORD64),
+        ('Dr3', DWORD64),
+        ('Dr6', DWORD64),
+        ('Dr7', DWORD64),
+        ('Rax', DWORD64),
+        ('Rcx', DWORD64),
+        ('Rdx', DWORD64),
+        ('Rbx', DWORD64),
+        ('Rsp', DWORD64),
+        ('Rbp', DWORD64),
+        ('Rsi', DWORD64),
+        ('Rdi', DWORD64),
+        ('R8', DWORD64),
+        ('R9', DWORD64),
+        ('R10', DWORD64),
+        ('R11', DWORD64),
+        ('R12', DWORD64),
+        ('R13', DWORD64),
+        ('R14', DWORD64),
+        ('R15', DWORD64),
+        ('Rip', DWORD64),
+        ('VectorRegister', M128A*26),
+        ('VectorControl', DWORD64),
+        ('DebugControl', DWORD64),
+        ('LastBranchToRip', DWORD64),
+        ('LastBranchFromRip', DWORD64),
+        ('LastExceptionToRip', DWORD64),
+        ('LastExceptionFromRip', DWORD64),
+   ]
